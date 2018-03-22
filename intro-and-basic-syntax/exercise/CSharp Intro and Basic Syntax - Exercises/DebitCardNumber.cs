6a2f45d15e8efa98df32091785f66fba50e52605
Petar Patev <petar1patev@gmail.com>
2018-03-22 09:17:17 +0200

    Added the C# Intro and Basic Syntax folder with a solution to Problem 01. Debit Card Number.AssemblyInfo.cs

---
+++ intro-and-basic-syntax/exercise/CSharp Intro and Basic Syntax - Exercises/DebitCardNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharp_Intro_and_Basic_Syntax___Exercises
+{
+    class DebitCardNumber
+    {
+        static void Main(string[] args)
+        {
+            var firstNum = int.Parse(Console.ReadLine());
+            var secondNum = int.Parse(Console.ReadLine());
+            var thirdNum = int.Parse(Console.ReadLine());
+            var fourthNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstNum.ToString("D4")} {secondNum.ToString("D4")} {thirdNum.ToString("D4")} {fourthNum.ToString("D4")}");
+        }
+    }
+}
