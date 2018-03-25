205dc6c2f9a7cdc89e82516744fd75dd75920a84
Petar Patev <petar1patev@gmail.com>
2018-03-25 22:40:05 +0300

    Added comments to problem 02. Rectangle area

--- intro-and-basic-syntax/02. Rectangle Area/problrm_02/Program.cs
+++ intro-and-basic-syntax/02. Rectangle Area/problrm_02/Program.cs
@@ -6,10 +6,14 @@ namespace Problem_2._Rectangle_Area
     {
         static void Main(string[] args)
         {
+            //We receive and parse rectangles width and height
             double width = double.Parse(Console.ReadLine());
             double height = double.Parse(Console.ReadLine());
+
+            // We calculate the area
             double area = height * width;
 
+            // Print the result to the console
             Console.WriteLine($"{area:F2}");
         }
     }
