37f09e6c6de638fcdc832946abfa5ecf7a3fb36c
PeterPenchev84 <penchev@abv.bg>
2018-03-25 20:37:30 +0300

    Added solution to problem 02. Rectangle Area

---
+++ intro-and-basic-syntax/02. Rectangle Area/problrm_02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Problem_2._Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = height * width;
+
+            Console.WriteLine($"{area:F2}");
+        }
+    }
+}
